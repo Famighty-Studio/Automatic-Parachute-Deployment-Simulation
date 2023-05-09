2943784a96868383e2011c208fc63abf66b655d2
har00zall <32787928+har00zall@users.noreply.github.com>
2023-05-09 10:44:11 +0530

    initial update

--- Assets/Scripts/Analyzer.cs
+++ Assets/Scripts/Analyzer.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class Analyzer : MonoBehaviour
 {
+    [Serializable] public enum AutoDeploymentParameter { Altitude, ImpactForce, TimeToGrounded}
+
     [Header("Object Properties")]
     [SerializeField] private float mass; // kg
     [SerializeField] private float parachuteSurfaceArea; // m2
@@ -20,7 +21,12 @@ public class Analyzer : MonoBehaviour
     [SerializeField] private float gravity = Physics.gravity.magnitude;
 
     [Header("Opening Load of The Parachute")]
-    [SerializeField] private float safeTimeToDeploy = 60f; // seconds
+	[SerializeField] private AutoDeploymentParameter autoDeploymentParameter;
+    [SerializeField] private float safealtitudeToDeploy = 2500f;
+    [SerializeField] private float safeImpactForceToDeploy = 20f;
+	[SerializeField] private float safeTimeToDeploy = 60f; // seconds
+    [SerializeField] private bool autoCalculateSafetyDeployment = false;
+    [Space]
     [SerializeField] private float localPressure; // Pa
     [SerializeField] private float canopyFillConstant; // constant
     [SerializeField] private float decellerationExponent; // constant
@@ -41,9 +47,10 @@ public class Analyzer : MonoBehaviour
     [SerializeField] private float dynamicViscosity; // kg/m/s || Pa s
 
     [Header("Debug")]
-    [SerializeField] private float weight;
-    [SerializeField] private float drag;
-    [SerializeField] private float timeLeftBeforeGrounded;
+    [SerializeField] private float weight; // N
+    [SerializeField] private float drag; // N
+    [SerializeField] private float timeLeftBeforeGrounded; // s
+    [SerializeField] private float maximumImpactForce; // N
 
     [Space]
     [SerializeField] private bool checkTerminalVelocity;
@@ -54,17 +61,28 @@ public class Analyzer : MonoBehaviour
     [SerializeField] private Transform parachuteTransform;
     [SerializeField] private ParachuteBehavior parachuteBehaviorComponent;
     [SerializeField] private SkinnedMeshRenderer parachuteMesh;
-    [SerializeField] private Rigidbody rigidbodyComponent;
+    [SerializeField] private CharacterController rigidbodyComponent;
+
+    private Vector3 motion;
+    private float cachedImpactForce;
+    private float initialAltitude; // m
 
     private void OnValidate()
     {
-		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody>();
-        rigidbodyComponent.mass = mass;
-        rigidbodyComponent.drag = loadDragCoefficient;
+        // Initiate the Character controller as a rigidbody component
+        // this controller will control the physics behavior of the load
+		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<CharacterController>();
     }
 
-    private void Update()
+	private void Start()
+	{
+        // set the initial altitude once the simulation is started
+        initialAltitude = transform.position.y;
+	}
+
+	private void Update()
     {
+        // manually open the parachute in case needed
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isParachuteOpen = true;
@@ -73,27 +91,28 @@ public class Analyzer : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // calculate the time left before the load is reached the ground
         if (!parachuteBehaviorComponent.Grounded) timeLeftBeforeGrounded = transform.position.y / velocity;
 
+        // cache the velocity before the parachute is inflated
         if (!isParachuteOpen)
         {
             velocityBeforeInflation = rigidbodyComponent.velocity.magnitude;
         }
 
+        // the inflation process
         if (isParachuteOpen && !inflated)
         {
             CalculateInflationTime();
             velocityAfterInflation = terminalVelocity;
 
-            if (parachuteMesh.GetBlendShapeWeight(0) > 0)
+			if (parachuteMesh.GetBlendShapeWeight(0) > 0)
             {
                 parachuteMesh.SetBlendShapeWeight(0,
                     Mathf.MoveTowards(parachuteMesh.GetBlendShapeWeight(0),
                     0,
                     (velocityBeforeInflation - velocityAfterInflation) / inflationTime)
                     );
-
-                parachuteTransform.localScale = Vector3.MoveTowards(parachuteTransform.localScale, Vector3.one, inflationTime * Time.deltaTime);
 
                 if (parachuteMesh.GetBlendShapeWeight(0) < 15)
                 {
@@ -103,54 +122,132 @@ public class Analyzer : MonoBehaviour
             else
             {
                 inflated = true;
+                printDragToConsole = true;
             }
         }
 
-        CalculatePhysics();
+        // manage the size of the parachute while its deploying
+        if (isParachuteOpen) 
+            parachuteTransform.localScale = 
+                Vector3.MoveTowards(parachuteTransform.localScale, Vector3.one, inflationTime * Time.deltaTime);
 
+        // calculate the physics behavior of the load
+		CalculatePhysics();
+
+        // calculate the auto deployment parameters
         CalculateAutoDeployment();
 
-        velocity = rigidbodyComponent.velocity.magnitude;
+        // debug only
         if (printDragToConsole) PrintDragValue();
         if (printWeightToConsole) PrintWeightValue();
+
+        // cache the impact force during the fall
+        cachedImpactForce = maximumImpactForce;
     }
 
     private void CalculatePhysics()
     {
+        // get the internal or the dynamic pressure as well as the terminal velocity of the load
         CalculateInternalPressure();
-        // CalculateInflationTime();
         CalculateTerminalVelocity();
-        
-        if (velocity > terminalVelocity)
+
+        // calculating impact force
+        maximumImpactForce =  (mass * Mathf.Pow(velocity, 2)) / (2 * initialAltitude * Time.deltaTime);
+
+        // make sure the velocity is calculated correctly and prevent the over flow
+		if (rigidbodyComponent.isGrounded)
         {
-            velocity = Mathf.MoveTowards(velocity, terminalVelocity, ((velocityBeforeInflation - velocityAfterInflation) / inflationTime) * Time.deltaTime);
-            rigidbodyComponent.velocity = Vector3.down * velocity;
+            velocity = 0;
         }
-        else rigidbodyComponent.velocity = Vector3.ClampMagnitude(rigidbodyComponent.velocity, terminalVelocity);
+        else
+        {
+			if (velocity > terminalVelocity)
+			{
+				velocity = Mathf.Lerp(velocity, terminalVelocity, (inflationTime) * Time.deltaTime);
+				print(Math.Abs(velocity - terminalVelocity));
+				if (Math.Abs(velocity - terminalVelocity) < 0.1f)
+				{
+					velocity = terminalVelocity - 0.1f;
+				}
+			}
+			else
+			{
+				velocity = Mathf.Lerp(velocity, terminalVelocity, gravity * Time.deltaTime * Time.deltaTime);
+			}
+		}
+        
+        // set the motion as the velocity is changing
+        motion = Vector3.down * velocity;
+
+        // apply the motion to the rigidbody component to control the load physics behavior
+		rigidbodyComponent.Move(motion * Time.deltaTime);
     }
 
     private void CalculateAutoDeployment()
     {
+        // recheck the terminal velocity
         CalculateTerminalVelocity();
 
-        float _safeTimeDeployment = safeTimeToDeploy - inflationTime;
-
-        if (timeLeftBeforeGrounded <= _safeTimeDeployment)
+        // only run the lines below based on the if condition
+        if (!autoCalculateSafetyDeployment)
         {
-            isParachuteOpen = true;
+            switch (autoDeploymentParameter)
+            {
+                case AutoDeploymentParameter.TimeToGrounded:
+                    {
+						float _safeTimeDeployment = safeTimeToDeploy - inflationTime;
+
+						if (timeLeftBeforeGrounded <= _safeTimeDeployment)
+						{
+							isParachuteOpen = true;
+						}
+
+                        break;
+					}
+                case AutoDeploymentParameter.Altitude:
+                    {
+						if (transform.position.y <= safealtitudeToDeploy)
+						{
+							isParachuteOpen = true;
+						}
+
+						break;
+					}
+                case AutoDeploymentParameter.ImpactForce:
+                    {
+						if (maximumImpactForce >= safeImpactForceToDeploy)
+						{
+							isParachuteOpen = true;
+						}
+
+						break;
+                    }
+            }
+        }
+        else
+        {
+            if (Mathf.Abs(velocity - terminalVelocity) < 0.1f && 
+                Mathf.Abs(timeLeftBeforeGrounded - (transform.position.y / velocity)) < 0.1f &&
+				Mathf.Abs(maximumImpactForce - cachedImpactForce) < 0.1f)
+            {
+				isParachuteOpen = true;
+			}
         }
     }
 
+    // calculating the internal or the dynamic pressure
     private void CalculateInternalPressure()
     {
         localPressure = (density * Mathf.Pow(velocity, 2)) / 2;
     }
 
+    // calculating the inflation time
     private void CalculateInflationTime()
     {
         inflationTime = canopyFillConstant * (chuteCanopyDiameter / Mathf.Pow(velocityBeforeInflation, decellerationExponent));
     }
 
+    // calculating the terminal velocity
     private void CalculateTerminalVelocity()
     {
         terminalVelocity = Mathf.Sqrt(
@@ -159,15 +256,99 @@ public class Analyzer : MonoBehaviour
             );
     }
 
+    // print the weight value to the console
     private void PrintWeightValue()
     {
-        weight = mass * (velocity / Time.deltaTime);
-		Debug.Log($"Weight: {weight}");
+        weight = mass * gravity;
+		// Debug.Log($"Weight: {weight}");
 	}
 
+    // print the drag value to the console
     private void PrintDragValue()
     {
-        drag = ((isParachuteOpen ? parachuteDragCoefficient : loadDragCoefficient) * density * Mathf.Pow(velocity, 2) * (isParachuteOpen ? parachuteSurfaceArea : loadSurfaceArea)) / 2;
-        Debug.Log($"Calculated Drag: {drag}");
-    }
+        if (isParachuteOpen)
+            drag = parachuteDragCoefficient * density * Mathf.Pow(velocity, 2) * parachuteSurfaceArea / 2;
+        else
+			drag = loadDragCoefficient * density * Mathf.Pow(velocity, 2) * loadSurfaceArea / 2;
+		// Debug.Log($"Calculated Drag: {drag}");
+	}
+
+    // this region is only to show the calculated values into the screen
+	private void OnGUI()
+	{
+        GUILayout.BeginHorizontal();
+
+        GUILayout.BeginVertical(); 
+        GUILayout.Label(" Initial Altitude (m): ");
+
+		GUILayout.Label(" Altitude (m): ");
+
+		GUILayout.Label(" Velocity (m/s): ");
+
+		GUILayout.Label(" Terminal Velocity (m/s): ");
+
+		GUILayout.Label(" Inflation Time (s): ");
+
+		GUILayout.Label(" Time Before Grounded (s): ");
+
+		GUILayout.Label(" Weight (N): ");
+
+		GUILayout.Label(" Drag Force (N): ");
+
+		GUILayout.Label(" Maximum Impact Force (N): ");
+
+		if (inflated)
+		{
+			GUILayout.Label("Dynamic Pressure (Pa): ");
+		}
+
+		GUILayout.EndVertical();
+		GUILayout.BeginVertical();
+		GUILayout.Label(initialAltitude.ToString());
+
+		GUILayout.Label(transform.position.y.ToString());
+
+		GUILayout.Label(velocity.ToString());
+
+		GUILayout.Label(terminalVelocity.ToString());
+
+		GUILayout.Label(inflationTime.ToString());
+
+		GUILayout.Label(timeLeftBeforeGrounded.ToString());
+
+		GUILayout.Label((mass * gravity).ToString());
+
+		GUILayout.Label(drag.ToString());
+
+		GUILayout.Label(maximumImpactForce.ToString());
+
+		if (inflated)
+		{
+			GUILayout.Label(localPressure.ToString());
+		}
+
+		GUILayout.EndVertical();
+		GUILayout.EndHorizontal();
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Simulation Speed");
+        if (GUILayout.Button("1x"))
+        {
+            Time.timeScale = 1;
+        }
+		if (GUILayout.Button("2x"))
+		{
+			Time.timeScale = 2;
+		}
+		if (GUILayout.Button("5x"))
+		{
+			Time.timeScale = 5;
+		}
+		if (GUILayout.Button("10x"))
+		{
+			Time.timeScale = 10;
+		}
+
+		GUILayout.EndHorizontal();
+	}
 }
